46ed39ad2437fb4f007f87b425dd67f02bfce29b
NiceTwice <onlynicetwice@gmail.com>
2023-06-26 10:42:28 -0300

    feat/points counter component

---
+++ app/components/Counter.js
@@ -0,0 +1,23 @@
+"use client"
+import { useContext } from "react";
+import { GameContext } from "@/context/gameContext";
+
+const Counter = () => {
+
+const { state } = useContext(GameContext)
+
+    return ( 
+        <article className="flex justify-center items-center w-full gap-x-60">
+            <div className="flex flex-col items-center gap-2 text-2xl font-bold font-serif">
+                <h3>Aves Rosas</h3>
+                <h3>{ state.pinkBirdPoints }</h3>
+            </div>
+            <div className="flex flex-col items-center gap-2 text-2xl font-bold font-serif">
+                <h3>Aves Azules</h3>
+                <h3>{ state.blueBirdPoints }</h3>
+            </div>
+        </article>
+     );
+}
+ 
+export default Counter;
