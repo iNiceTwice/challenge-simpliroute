5930bb0ec23a61ff7fe0568980a0869023805e7a
NiceTwice <onlynicetwice@gmail.com>
2023-06-26 13:25:55 -0300

    feat/main page with all game components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import { GameProvider } from '@/context/gameProvider'
+import Board from './components/Board'
+import Counter from './components/Counter'
+import Timer from './components/Timer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24 bg-main">
-      
-    </main>
+    <GameProvider>
+      <main className="flex flex-col w-full min-h-screen items-center justify-center">
+        <section className="w-full">
+          <Counter/>
+        </section>
+        <section className='flex justify-center items-center w-full'>
+          <Timer/>
+        </section>
+        <section className="p-8 w-full flex items-center justify-center">
+          <Board/>
+        </section>
+      </main>
+    </GameProvider>
   )
 }
